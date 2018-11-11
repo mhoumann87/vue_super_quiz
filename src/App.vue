38e89340654d887927071577b2f4b01b0b6bd478
Michael Houmann <michael@michael-h.dk>
2018-11-11 12:44:21 +0100

    Add gamelogic and animations

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,66 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+<h1 class="text-center">Super Quiz</h1>
+<transition name="flip" mode="out-in">
+<component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-questions'"></component>
+</transition>
+</div>
+</div>
+</div>
 </template>
 
 <script>
+import Answers from "./components/Answers";
+import Questions from "./components/Questions";
+
 export default {
-  name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      mode: "app-questions"
+    };
+  },
+  methods: {
+    answered(isCorrect) {
+      if (isCorrect) {
+        this.mode = "app-answers";
+      } else {
+        this.mode = "app-questions";
+        alert("Wrong Answer, Try Again");
+      }
     }
+  },
+  components: {
+    "app-answers": Answers,
+    "app-questions": Questions
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.flip-enter-active {
+  animation: flip-in 0.5s ease-in forwards;
+}
+.flip-leave-active {
+  animation: flip-out 0.5s ease-in forwards;
 }
 
-h1, h2 {
-  font-weight: normal;
+@keyframes flip-out {
+  from {
+    transform: rotateY(0deg);
+  }
+  to {
+    transform: rotateY(90deg);
+  }
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+@keyframes flip-in {
+  from {
+    transform: rotateY(90deg);
+  }
+  to {
+    transform: rotateY(0deg);
+  }
 }
 </style>
